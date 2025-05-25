220a18bcc70b679e6c378f6fddd45505db02a6f2
ownleex <145456737+ownleex@users.noreply.github.com>
2025-05-25 15:32:32 +0200

    Ajout de la classe "whatever" avec des fonctions utilitaires et création du Makefile.

--- ex00/include/whatever.hpp
+++ ex00/include/whatever.hpp
@@ -6,7 +6,28 @@
 /*   By: ayarmaya <ayarmaya@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 14:02:31 by ayarmaya          #+#    #+#             */
-/*   Updated: 2025/05/25 14:02:33 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2025/05/25 15:29:11 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+template <typename T>
+void swap(T& a, T& b) {
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+template <typename T>
+const T& min(const T& a, const T& b) {
+    return (a < b) ? a : b;
+}
+
+template <typename T>
+const T& max(const T& a, const T& b) {
+    return (a > b) ? a : b;
+}
+
+#endif
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,30 @@
 /*   By: ayarmaya <ayarmaya@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 14:02:48 by ayarmaya          #+#    #+#             */
-/*   Updated: 2025/05/25 14:02:49 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2025/05/25 15:29:42 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+#include "whatever.hpp"
+
+int main(void) {
+    int a = 2;
+    int b = 3;
+    
+    ::swap(a, b);
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+    std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+    
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+    std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+    
+    return 0;
+}
