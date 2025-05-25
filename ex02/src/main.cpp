59c4f531f124370507a3145a68e8d6ccf4d06325
ownleex <145456737+ownleex@users.noreply.github.com>
2025-05-26 01:23:21 +0200

    Refactor main.cpp: amélioration des tests de la classe Array et correction des messages d'erreur

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,68 +6,64 @@
 /*   By: ayarmaya <ayarmaya@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 00:45:58 by ayarmaya          #+#    #+#             */
-/*   Updated: 2025/05/26 00:47:35 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2025/05/26 01:16:46 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <time.h>
 #include "Array.hpp"
 
 #define MAX_VAL 750
 
-int main(int, char**)
+int main()
 {
+    // 1) Test du constructeur par défaut et size()
+    Array<int> empty;
+    std::cout << "empty.size() = " << empty.size() << std::endl;
+    try {
+        empty[0];
+    } catch (const std::exception& e) {
+        std::cerr << "empty[0] : " << e.what() << std::endl;
+    }
+
+    // 2) Test de l'initialisation et de size()
     Array<int> numbers(MAX_VAL);
+    std::cout << "numbers.size() = " << numbers.size() << std::endl;
+
+    // 3) Remplissage et miroir pour vérifier deep copy
     int* mirror = new int[MAX_VAL];
     srand(time(NULL));
-    
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        const int value = rand();
-        numbers[i] = value;
-        mirror[i] = value;
+    for (int i = 0; i < MAX_VAL; i++) {
+        numbers[i] = mirror[i] = rand();
     }
-    
-    // SCOPE
     {
         Array<int> tmp = numbers;
         Array<int> test(tmp);
     }
-
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
-            std::cerr << "didn't save the same value!!" << std::endl;
+    for (int i = 0; i < MAX_VAL; i++) {
+        if (mirror[i] != numbers[i]) {
+            std::cerr << "Erreur de copie profonde !" << std::endl;
             return 1;
         }
     }
-    
-    try
-    {
-        numbers[-2] = 0;
+
+    // 4) Test exceptions hors bornes
+    try { numbers[-2] = 0; } catch(const std::exception& e) {
+        std::cerr << "numbers[-2] : " << e.what() << std::endl;
     }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    
-    try
-    {
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
+    try { numbers[MAX_VAL] = 0; } catch(const std::exception& e) {
+        std::cerr << "numbers[MAX_VAL] : " << e.what() << std::endl;
     }
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        numbers[i] = rand();
-    }
-    
+    // 5) Test sur un type non trivial
+    Array<std::string> words(3);
+    words[0] = "foo"; words[1] = "bar"; words[2] = "baz";
+    for (unsigned i = 0; i < words.size(); i++)
+        std::cout << words[i] << ' ';
+    std::cout << std::endl;
+
     delete [] mirror;
     return 0;
 }
